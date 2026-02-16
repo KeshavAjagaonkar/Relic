577fc8e45d6b9ba12fcf34364803a483c3a00649
KeshavAjagaonkar <keshavvamanajagaonkar@gmail.com>
2026-02-16 13:06:27 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,49 @@
-import yargs from "yargs";
-import {hideBin } from "yargs/helpers";
+import yargs, { describe } from "yargs";
+import { hideBin } from "yargs/helpers";
 
 import { initRepo } from "./controllers/init.js";
+import { addRepo } from "./controllers/add.js";
+import { commitRepo } from "./controllers/commit.js";
+import { pushRepo } from "./controllers/push.js";
+import { pullRepo } from "./controllers/pull.js";
+import { revertRepo } from "./controllers/revert.js";
 
-yargs(hideBin(process.argv)).command(
-  "init",
-  "Initialise a new repository",
-  {},
-  initRepo,
-).demandCommand(1, "at least one command")
-    .help()
-    .argv;
-
-
+yargs(hideBin(process.argv))
+  .command("init", "Initialise a new repository", {}, initRepo)
+  .command(
+    "add <file>",
+    "Add a file to the repository",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "File to add the stagging area",
+        type: "string",
+      });
+    },
+    addRepo,
+  )
+  .command(
+    "commit <message>",
+    "commit the stagged file",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "Commit message",
+        type: "string",
+      });
+    },
+    commitRepo,
+  )
+  .command("push", "push commits to s3", {}, pushRepo)
+  .command("pull", "Pull commits from s3", {}, pullRepo)
+  .command(
+    "revert <commitID>",
+    "Revert a commit to specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "commitID to revert to",
+        type: "string",
+      });
+    },
+    revertRepo
+  )
+  .demandCommand(1, "at least one command")
+  .help().argv;
