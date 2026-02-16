bbf122ff009edd7d7429767e03fba3ea3dfe1a88
KeshavAjagaonkar <keshavvamanajagaonkar@gmail.com>
2026-02-16 16:43:48 +0530

    initialisition of repo logic is done

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,22 @@
-function initRepo() {
+import fs from "node:fs/promises";
+import path from "node:path";
+async function initRepo() {
+    const repoPath=path.resolve(process.cwd(),".relic");
+    const commitPath = path.join(repoPath, "commits");
+
+    try {
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitPath, { recursive: true });
+        await fs.writeFile(
+            path.join(repoPath, "config.json"),
+            JSON.stringify({ bucket: "S3 bucket " })
+        );
+        console.log("Repo initialised");
+    }
+    catch (err) {
+        console.error("Error in initialising the repo", err);
+    }
     console.log("init command initialised");
 }
-
+    
 export {initRepo};
